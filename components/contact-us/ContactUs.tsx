1ad7f8e7c35c83a79c3f4f0a62f661f5d0708aa5
payalptoliya416 <payal.rising416@gmail.com>
2026-08-10 11:33:48 +0530

    pages hero section minus margin top changes

--- components/contact-us/ContactUs.tsx
+++ components/contact-us/ContactUs.tsx
@@ -7,7 +7,7 @@ function ContactUs() {
   const { companyName } = useSettings();
 
   return (
-    <section className="p-5 -mt-[116px] md:-mt-[145px] pb-0">
+    <section className="p-5 -mt-[148px] sm:-mt-[167px] md:-mt-[179px] pb-0">
       <motion.div
         initial={{ opacity: 0, scale: 0.98 }}
         whileInView={{ opacity: 1, scale: 1 }}
@@ -15,7 +15,7 @@ function ContactUs() {
         transition={{ duration: 0.7, ease: "easeOut" }}
         className="relative rounded-3xl overflow-hidden min-h-[410px] bg-cover bg-center bg-[url(/assets/images/contact-bg.png)] flex justify-center items-center md:bg-[length:100%_100%] bg-no-repeat"
       >
-        <div className="relative z-10 pt-[132px] md:pt-[127px] pb-14 md:pb-[67px]">
+        <div className="relative pt-[132px] md:pt-[142px] xl:pt-[127px] pb-14 md:pb-[67px]">
           <div className="container-custom text-center text-white">
             <motion.h1
               initial={{ opacity: 0, y: 50 }}
--- components/services/ServicesHero.tsx
+++ components/services/ServicesHero.tsx
@@ -2,11 +2,11 @@
 
 function ServicesHero() {
   return (
-   <section className="p-5 -mt-[116px] md:-mt-[145px] pb-0">
+   <section className="p-5 -mt-[148px] sm:-mt-[167px] md:-mt-[179px] pb-0">
       <div
         className="relative rounded-3xl overflow-hidden min-h-[410px] bg-cover bg-center bg-[url(/assets/images/service.png)] flex justify-center items-center md:bg-[length:100%_100%] bg-no-repeat"
       >
-        <div className="relative z-10 pt-[132px] md:pt-[127px] pb-14 md:pb-[67px]">
+        <div className="relative z-10 pt-[132px] md:pt-[142px] xl:pt-[127px] pb-14 md:pb-[67px]">
           <div className="container-custom text-center text-white">
             <h1 className="mx-auto max-w-5xl text-center text-[28px] font-extrabold leading-[40px] sm:text-[42px] sm:leading-[52px] md:text-[48px] md:leading-[58px] lg:text-[60px] lg:leading-[72px] mb-5">
             Reliable Solutions for {" "}
